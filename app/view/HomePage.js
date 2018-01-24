fa04076ab449656d505e7aab61311141022290c2
wangchaoyi <wangchaoyi@betterfun.net>
2018-01-24 20:01:06 +0800

    添加日历和图片。

--- app/view/HomePage.js
+++ app/view/HomePage.js
@@ -8,33 +8,72 @@ import {
     Text,
     View,
     TextInput,
-    TouchableOpacity
+    TouchableOpacity,
+	ScrollView,
+	Dimensions
 } from 'react-native';
+import {Calendar} from 'react-native-calendars';
+import Echarts from 'native-echarts';
+const {width} = Dimensions.get('window');
 
 class HomePage extends Component<{}> {
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this._onDayPress = this._onDayPress.bind(this);
+	}
+	
     render() {
+	    const option = {
+		    title: {
+			    text: 'ECharts demo'
+		    },
+		    tooltip: {},
+		    legend: {
+			    data:['销量']
+		    },
+		    xAxis: {
+			    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+		    },
+		    yAxis: {},
+		    series: [{
+			    name: '销量',
+			    type: 'bar',
+			    data: [5, 20, 36, 10, 10, 20]
+		    }]
+	    };
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    欢迎来到喂养日志
-                </Text>
-            </View>
+	        <ScrollView style={styles.scrollContainer}>
+                <Calendar
+                    onDayPress={this._onDayPress}
+                    style={styles.calendar}
+                    hideExtraDays
+                    markedDates={{[this.state.selected]: {selected: true}}}
+                />
+		        <Echarts option={option} height={300} width={width} />
+	        </ScrollView>
         );
     }
+	
+	_onDayPress(day) {
+		this.setState({
+			selected: day.dateString
+		});
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#ffffff',
-    },
-    welcome: {
-        fontSize: 30,
-        textAlign: 'center',
-        margin: 10,
-    },
+	scrollContainer: {
+		flex: 1,
+		backgroundColor: '#7d7d7d'
+	},
+	calendar: {
+		borderTopWidth: 1,
+		paddingTop: 5,
+		borderBottomWidth: 1,
+		borderColor: '#eee',
+		height: 350
+	},
 });
 
 module.exports = HomePage;
