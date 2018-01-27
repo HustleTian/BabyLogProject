3a644bbe35d67fb3477cd509e287d16709b8d336
wangchaoyi <wangchaoyi@betterfun.net>
2018-01-27 12:16:12 +0800

    Revert "添加新的插件。"
    This reverts commit 9486d063b1a56e21a5fe5ac49c4eee6130a31f62.

--- app/view/Component/EventComponent.js
+++ app/view/Component/EventComponent.js
@@ -7,86 +7,17 @@ import {
     StyleSheet,
     Text,
     View,
-    Button,
     TouchableOpacity,
 } from 'react-native';
-import ModalDropdown from 'react-native-modal-dropdown';
-import DateTimePicker from 'react-native-datetime';
 
 class EventComponent extends Component<{}> {
-	constructor(props) {
-		super(props);
-		this._getOptions = this._getOptions.bind(this);
-		this._onSelect = this._onSelect.bind(this);
-		this.state = {date: new Date()};
-	}
-	
     render() {
         return (
             <View style={styles.container}>
-                <ModalDropdown
-                    options={this._getOptions()}
-                    defaultValue="活动1"
-                    defaultIndex={0}
-                    onSelect={this._onSelect()}
-                />
-                <Text style={{textAlign: 'center'}}>
-		            test test test
-                </Text>
-                <Text style={{textAlign: 'center'}}>
-		            {this.state.date.toString()}
-                </Text>
-                <View style={{height:40}} />
-                <Button onPress={this.showDatePicker}>
-                    <Text showDatePicker/>
-                </Button>
-                <View style={{height:40}} />
-                <Button onPress={this.showTimePicker}>
-                    <Text showDatePicker/>
-                </Button>
-                <View style={{height:40}} />
-                <Button onPress={this.showDateTimePicker}>
-                    <Text showDateTimePicker/>
-                </Button>
-                <DateTimePicker ref={(picker)=>{this.picker=picker}}/>
+
             </View>
         );
     }
-    
-    _getOptions() {
-        return ['option 1', 'option 2', 'option 3'];
-    }
-    
-    _onSelect() {
-    
-    }
-	
-	getInitialState() {
-		return {
-			date: new Date(),
-		}
-	}
-	
-	showDatePicker() {
-		var date = this.state.date;
-		this.picker.showDatePicker(date, (d)=>{
-			this.setState({date:d});
-		});
-	}
-	
-	showTimePicker() {
-		var date = this.state.date;
-		this.picker.showTimePicker(date, (d)=>{
-			this.setState({date:d});
-		});
-	}
-	
-	showDateTimePicker() {
-		var date = this.state.date;
-		this.picker.showDateTimePicker(date, (d)=>{
-			this.setState({date:d});
-		});
-	}
 }
 
 const styles = StyleSheet.create({
